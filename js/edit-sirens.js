b63100ee3fead785c24771ba994e544b7ff0337c
Lukas Balthazar <LBTurner@outlook.dk>
2025-11-06 19:43:36 +0100

    Ended up deleting a bunch of code while tired which im unable to recover. Im pushing this to github to try and salvage something.

--- js/edit-sirens.js
+++ js/edit-sirens.js
@@ -1,4 +1,4 @@
-import {fetchAnyUrl, restDelete, postObjectAsJson} from "./modulejson.js";
+import {fetchAnyUrl, restDelete, postObjectAsJson, putObjectAsJson, getObjectAsJson} from "./modulejson.js";
 
 document.addEventListener("DOMContentLoaded", initForm);
 let sirenForm
@@ -15,7 +15,7 @@ async function initForm() {
     const siren = await fetchSiren(sirenId);
     if(siren) {
         fillForm(siren)
-        sirenForm.action = `http://localhost:8080/sirens/${sirenId}`;
+        sirenForm.action = `http://localhost:8080/sirens/${requestedId}`;
     }
 
     sirenForm.addEventListener("submit", handleFormSubmit);
@@ -23,9 +23,15 @@ async function initForm() {
 
 
 
-async function fetchSiren(id){
-    const response = await fetch(`http://localhost:8080/sirens/${id}`);
-    return await response.json();
+async function fetchSiren(requestedId){
+    try {
+        const response = await fetch(`http://localhost:8080/sirens/${requestedId}`);
+        if (!response.ok) throw new Error("Siren not found");
+        return await response.json();
+    } catch (err) {
+        console.error("Error fetching siren:", err);
+        alert("Was unable to fetch siren data")
+    }
 }
 
 function fillForm(siren) {
@@ -34,6 +40,31 @@ function fillForm(siren) {
     sirenForm.longitude.value = siren.longitude;
     sirenForm.status.value = siren.status;
     sirenForm.disabled.value = siren.disabled;
-    sirenForm.lastStatusChange.value = siren.lastStatusChange;
+}
 
+async function handleFormSubmit(event) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const url = form.action
+    try {
+        const formData = new FormData(form)
+        const response = await putFormDataAsJson(url, formData)
+        if(response.ok) {
+            window.location.href = "all-sirens.html"
+        }
+    } catch (error)
+    {
+        alert(error.message)
+        console.log(error)
+    }
+}
+async  function putFormDataAsJson(url, formData) {
+    console.log(url)
+    console.log(formData)
+    const plainFormData = Object.fromEntries(formData.entries())
+    const resp = await getObjectAsJson(url, plainFormData, "GET")
+    return  resp
+} async function parseJsonOrNull(r) {
+    const text = await r.text();
+    return text ? JSON.parse(text) : null;
 }
